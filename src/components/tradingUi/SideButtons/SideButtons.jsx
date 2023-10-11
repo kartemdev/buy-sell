8228f259bc72bb1fab77979c665a6e3dc83b98e9
kartemdev <ihctuyd@mail.ru>
2023-10-11 07:58:54 +0300

    fixed: fix dynamic update choosen pair prices

--- src/components/tradingUi/SideButtons/SideButtons.jsx
+++ src/components/tradingUi/SideButtons/SideButtons.jsx
@@ -1,41 +1,48 @@
+import { useEffect, useState } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
-// import { setPrices } from '../../../redux/slices/chosenCurrencyPairSlice';
+import { setPrices } from '../../../redux/slices/chosenCurrencyPairSlice';
 import { onOpen } from '../../../redux/slices/uiSlices/modalSlice';
 import styles from './SideButtons.module.scss';
 
 function SideButtons() {
   const { chosenPair } = useSelector((store) => store.chosenCurrencyPair);
-
-  // const { list } = useSelector((store) => store.currenciesPairs);
+  const { list } = useSelector((store) => store.currenciesPairs);
 
   const dispatch = useDispatch();
 
-  // Код который меняет рандомно цену, приложение не сломает, но работает плохо
-  // Для теста раскоментируйте строчку импорта setPrices и хука useSelector выше
+  useEffect(() => {
+    if (!chosenPair.name) {
+      return;
+    }
 
-  // if (chosenPair.name) {
-  //   const staticPair = list.filter((pair) => pair.name === chosenPair.name);
+    const staticPair = list.find((pair) => pair.name === chosenPair.name);
 
-  //   const result = {
-  //     sidePriceBuy: +(Math.random() * ((staticPair[0].sidePriceBuy + 0.02)
-  //        - (staticPair[0].sidePriceBuy - 0.02))
-  //         + staticPair[0].sidePriceBuy - 0.02)
-  //       .toFixed(staticPair[0].sidePriceBuy.toString().split('.')[1].length),
-  //     sidePriceSell: +(Math.random() * ((staticPair[0].sidePriceSell + 0.02)
-  //     - (staticPair[0].sidePriceSell - 0.02))
-  //      + staticPair[0].sidePriceSell - 0.02)
-  //       .toFixed(staticPair[0].sidePriceSell.toString().split('.')[1].length),
-  //   };
-  //   const idTimeout = setTimeout(() => {
-  //     dispatch(setPrices(result));
-  //   }, 1000 * 3);
-  // }
+    const result = {
+      sidePriceBuy: +(Math.random() * ((staticPair.sidePriceBuy + 0.02)
+         - (staticPair.sidePriceBuy - 0.02))
+          + staticPair.sidePriceBuy - 0.02)
+        .toFixed(staticPair.sidePriceBuy.toString().split('.')[1].length),
+      sidePriceSell: +(Math.random() * ((staticPair.sidePriceSell + 0.02)
+      - (staticPair.sidePriceSell - 0.02))
+       + staticPair.sidePriceSell - 0.02)
+        .toFixed(staticPair.sidePriceSell.toString().split('.')[1].length),
+    };
+
+    const id = setTimeout(() => {
+      dispatch(setPrices({ ...chosenPair, ...result }));
+    }, 1000 * 2);
+
+    // eslint-disable-next-line consistent-return
+    return () => {
+      clearTimeout(id);
+    };
+  }, [chosenPair.sidePriceBuy, chosenPair.sidePriceSell]);
 
   return (
     <div className={styles.btnContainer}>
       <button
         type="button"
-        className={chosenPair.name ? null : `${ styles.disabled}`}
+        className={chosenPair.name ? null : `${styles.disabled}`}
         onClick={() => dispatch(onOpen('BUY'))}
       >
         BUY<br />
@@ -43,7 +50,7 @@ function SideButtons() {
       </button>
       <button
         type="button"
-        className={chosenPair.name ? null : `${ styles.disabled}`}
+        className={chosenPair.name ? null : `${styles.disabled}`}
         onClick={() => dispatch(onOpen('SELL'))}
       >
         SELL<br />
